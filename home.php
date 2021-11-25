a6fe97b8d375a01360e825878ccef536f7c1982e
Karan Singh Manral <66790855+KaranManral@users.noreply.github.com>
2021-11-25 17:19:52 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,27 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="home_style.css">
-</head>
-<body>
-	<center>
-		<div>
-			<iframe src=""></iframe>
-		</div>
-		<div>
-			<iframe src=""></iframe>
-		</div>
-		<div>
-			<iframe src=""></iframe>
-		</div>
-		<div>
-			<iframe src=""></iframe>
-		</div>
-		<div>
-			<iframe src=""></iframe>
-		</div>
-	</center>
-</body>
-</html>
+<?php
+	session_start();
+	if(!isset($_SESSION['User_Logged_In']))
+		echo "<script>parent.location.href='index.php';</script>";
+	else
+		echo "<script>location.href='https://professor91.github.io/SyllabusWeb_NewDesign_Preview/stages/ug.html';</script>";
+?>
