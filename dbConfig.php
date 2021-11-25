3fd37c3359059ce3c795d2accbb8bce0d617c7e7
Karan Singh Manral <66790855+KaranManral@users.noreply.github.com>
2021-11-25 17:05:36 +0530

    Update dbConfig.php

--- dbConfig.php
+++ dbConfig.php
@@ -4,8 +4,20 @@
     $password = "";
     $dbname = "profile";
 
-    $conn=new mysqli($servername, $username, $password, $dbname);
+    $conn=new mysqli($servername, $username, $password);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
+    }
+
+    $db_selected=mysqli_select_db($conn,$dbname);
+    if(!$db_selected)
+    {
+        $sql_query="CREATE DATABASE $dbname";
+        if(mysqli_query($conn,$sql_query))
+        {
+
+        }
+        else
+            echo "Error creating database.".mysqli_error($conn);
     }
 ?>
