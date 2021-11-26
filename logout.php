7aae87b185b9b7776913de3856880477bd154a91
Karan Singh Manral <66790855+KaranManral@users.noreply.github.com>
2021-11-27 02:08:25 +0530

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+    if(isset($_SESSION['User_Logged_In']))
+    {
+        session_destroy();
+        echo "<script>parent.location.href='index.php';</script>";
+    }
+    else
+    {
+        echo "<script>parent.location.href='index.php';</script>";
+    }
+?>
