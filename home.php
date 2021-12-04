90cfa1f4e9223cdadd34e25b8ebd1368ba965984
Karan Singh Manral <66790855+KaranManral@users.noreply.github.com>
2021-12-04 17:01:58 +0530

    Delete home.php

--- home.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start();
-	if(!isset($_SESSION['User_Logged_In']))
-		echo "<script>parent.location.href='index.php';</script>";
-	else
-		echo "<script>location.href='https://professor91.github.io/SyllabusWeb_NewDesign_Preview/stages/ug.html';</script>";
-?>
